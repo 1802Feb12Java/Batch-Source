7c0b20e7b5a6fbe68e6b43002e16ed6c8b288826
nknguy <32646451+nknguy@users.noreply.github.com>
2018-02-12 16:44:08 -0500

    Added Day1Examples folder

---
+++ Day1Examples/src/com/revature/Basics.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//naming convention
+				//Classes and Projects-Pascal Case, Capitalize the first letter of every word
+				//	Ex: FirstSecondThird
+				// Methods and variable: camel casing EX: firstSencondThird
+				// Packages: lower case separated by periods EX. com.revature
+				// Constants: ALL CAPS!!! delimited by underscores FIRST_SECOND_THIRD
+	}
+
+}
