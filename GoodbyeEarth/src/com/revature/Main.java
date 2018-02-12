a9339e1591489518e8858cdcbf9e119ae668b616
computerGuy86 <dominicnguyen2015@gmail.com>
2018-02-12 14:31:48 -0500

    GoodByeEarth Dominic

--- GoodbyeEarth/src/com/revature/Main.java
+++ GoodbyeEarth/src/com/revature/Main.java
@@ -3,7 +3,7 @@ package com.revature;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Dominic Nguyen did a thing!");
+		System.out.println("Dominic did a thing!");
 
 	}
 
