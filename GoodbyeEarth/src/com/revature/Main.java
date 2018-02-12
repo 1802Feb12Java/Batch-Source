c88389f3d709f282e9fea45b2e3ce14512a29f64
mmiocic <mmiocic0514@gmail.com>
2018-02-12 14:34:01 -0500

    Marko did a thing

--- GoodbyeEarth/src/com/revature/Main.java
+++ GoodbyeEarth/src/com/revature/Main.java
@@ -3,7 +3,7 @@ package com.revature;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Zain did a thing!");
+		System.out.println("Marko did a thing!");
 
 	}
 
