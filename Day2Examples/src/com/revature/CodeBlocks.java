1ee1e894fc804f3026c60c5f16545e9c20ca576e
Joshua Stark <jpstark25@gmail.com>
2018-02-13 14:16:09 -0500

    More things today

---
+++ Day2Examples/src/com/revature/CodeBlocks.java
@@ -0,0 +1,17 @@
+package com.revature;
+
+public class CodeBlocks {
+	
+	//instance code block that executes before constructor
+	//when object is instantiated. Note that we can't reference the 
+	//object using "this", since the object isn't instantiated yet.
+	
+	//{...}
+	
+	//Static code blocks are similar.
+	//These execute once, when the class is loaded in JVM.
+	/* These code blocks give us a window upon the creation of these objects.
+	 * We can use these code blocks to inject behavior when we instantiate objects.
+	 */
+	
+}
