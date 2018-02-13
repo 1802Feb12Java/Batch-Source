24964d9da7c1ead72bb02d897019b577acbf9a89
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-02-13 09:01:48 -0500

    Pull from KnightenM for Day1 Notes

---
+++ Day1Examples/src/com.revature/Basics.java
@@ -0,0 +1,30 @@
+//Naming Convention
+/**
+ *  Class and Projects -> Pascal Case
+ *  cap the first letter of every word
+ *  i.e. FirstClass
+ **/
+
+/**
+ *  Methods and Variables -> Camel Case
+ *  i.e. firstVariable
+ **/
+
+/**
+ *  Packages -> all lower case separate by '.'
+ *  com.revature
+ **/
+
+/**
+ *  Constants -> All CAP, delimited by underscores
+ *  FIRST_CONST
+ **/
+
+package com.revature;
+
+public class Basics {
+
+	public static void main(String[] args) {
+				
+	}
+}
--- GoodbyeEarth/src/com/revature/Main.java
+++ GoodbyeEarth/src/com/revature/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Kevin did a thing!");
-
+		
 	}
 
 }
