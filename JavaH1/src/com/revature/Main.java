0dd41df2203c654fbcf95bc8e3485754ea1415f9
Josh Boudreau <jboud1217@gmail.com>
2018-02-16 09:41:57 -0500

    Minor changes

--- JavaH1/src/com/revature/Main.java
+++ JavaH1/src/com/revature/Main.java
@@ -22,6 +22,21 @@ public class Main {
 		
 		// Question six - determine if given number is even
 		new Q6(30);
+		
+//		StringBuilder b1 = new StringBuilder("snorkler");
+//		StringBuilder b2 = new StringBuilder("yoodler");
+//		
+//		b1.append(b2.substring(2,5).toUpperCase());
+//		b2.insert(3, b1.append("a"));
+//		b1.replace(3, 4, b2.substring(4)).append(b2.append(false));
+//		
+//		System.out.println(b1);
+//		System.out.println(b2);
+		
+//		int i = 1234567890;
+//		float f = i;
+//		System.out.println(i - (int)f);
+		
 	}
 
 }
