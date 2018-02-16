657cfb4afb107934687a6fd55590b8240b2ef636
mmiocic <mmiocic0514@gmail.com>
2018-02-16 10:38:58 -0500

    please commit

--- Test/src/main/java/com/revature/Driver.java
+++ Test/src/main/java/com/revature/Driver.java
@@ -9,8 +9,8 @@ public class Driver {
 		// TODO Auto-generated method stub
 
 		Guitar gibson= new Guitar(1,"Les Paul", "Red",1500.35);
-		//System.out.println(gibson.toString());
-		//SerializeDemo.writeObject(gibson, "out.txt");
+		System.out.println(gibson.toString());
+		SerializeDemo.writeObject(gibson, "out.txt");
 		Guitar gib=(Guitar) SerializeDemo.readObject("out.txt");
 		System.out.println(gib.getName());
 	}
