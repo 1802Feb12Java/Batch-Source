a6f47cbe326f51994ed919900b8870f4d7e03dec
Scott Bennett <scott.g.bennett@gmail.com>
2018-02-18 10:16:49 -0500

    Add comments to Q9.

--- CoreJavaAssignment/src/com/revature/corejava/Question9.java
+++ CoreJavaAssignment/src/com/revature/corejava/Question9.java
@@ -7,12 +7,12 @@ public class Question9 {
 	public static void printPrimes() {
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		for (int i = 1; i <= 100; i++) {
+		for (int i = 1; i <= 100; i++) { //add numbers to array
 			numbers.add(i);
 		}
 		System.out.println("Initial ArrayList created!\nInitial Array:");
 		System.out.println(Arrays.toString(numbers.toArray()));
-		for (int number : numbers) {
+		for (int number : numbers) { //use PrimeFinder class to find primes and add to primes array
 			if(PrimeFinder.isPrime(number)) {
 				primes.add(number);
 			}
