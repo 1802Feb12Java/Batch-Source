7e153927225a04cb2cef27419ce32789e53e57de
Kevin Green <kgreen@nevada.unr.edu>
2018-02-20 05:25:43 -0500

    Account class updated

--- BankingAppPart1/src/com/revature/accounts/Account.java
+++ BankingAppPart1/src/com/revature/accounts/Account.java
@@ -4,4 +4,32 @@ public class Account {
 	private double balance;
 	private boolean active;
 	
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void transfer(double amount, Account destination) {
+		balance -= amount;
+		destination.setBalance(destination.getBalance() + amount);		
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 }
