d14b297932a1c2a60faac6997045438edc26abca
Josh Boudreau <jboud1217@gmail.com>
2018-02-21 22:33:44 -0500

    Added wrong folders

--- Week1_HW/src/q18/stringmanipulation/StringManipulation.java
+++
@@ -1,8 +0,0 @@
-package q18.stringmanipulation;
-
-public abstract class StringManipulation {
-	public abstract boolean checkUppercase();
-	public abstract void convertLowercaseToUppercase();
-	public abstract void convertStringToInt();
-}
-
