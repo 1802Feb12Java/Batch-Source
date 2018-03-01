1ce1bfd6f2d2f2ba2a48c83bb4ad018f573f2b4b
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-03-01 13:21:34 -0500

    clean up

--- Day4Examples/src/com/revature/classtypes/Fishes.java
+++
@@ -1,9 +0,0 @@
-package com.revature.classtypes;
-
-public abstract class Fishes implements InterfaceFun{
-	public static int numofFins=2;
-	public abstract void swim(int speed);
-	public abstract void eat();
-	
-	
-}
