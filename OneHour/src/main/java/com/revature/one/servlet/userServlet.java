2ae29809000fe26a2c3fd870d0247bb27e755639
Kevin Green <kgreen@nevada.unr.edu>
2018-03-07 15:54:51 -0500

    OneHour, might be broken

---
+++ OneHour/src/main/java/com/revature/one/servlet/userServlet.java
@@ -0,0 +1,68 @@
+package com.revature.one.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.revature.one.dao.impl
+
+/**
+ * Servlet implementation class userServlet
+ */
+public class userServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public userServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserDaoImpl userDao= new UserDaoImpl();
+		User user = new User();
+		JSONObject userRequest = new JSONObject();
+		
+		user.getName() = userRequest.get("name");
+		user.getID() = userRequest.get("id");
+		
+		
+		StringBuffer jb = new StringBuffer();
+		  String line = null;
+		  try {
+		    BufferedReader reader = request.getReader();
+		    while ((line = reader.readLine()) != null)
+		      jb.append(line);
+		  } catch (Exception e) { success = 0;}
+
+		  try {
+		    JSONObject jsonObject =  HTTP.toJSONObject(jb.toString());
+		  } catch (JSONException e) {
+		    // crash and burn
+		    throw new IOException("Error parsing JSON request string");
+		  }
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
