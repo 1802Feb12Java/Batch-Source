1b1ebeb235dfde3fc637c26a44ec18c03d1423e0
mmiocic <mmiocic0514@gmail.com>
2018-03-07 14:52:33 -0500

    updating Test to leave my branch

--- Test/src/main/java/com/revature/Driver.java
+++ Test/src/main/java/com/revature/Driver.java
@@ -11,16 +11,14 @@ public class Driver {
 		// TODO Auto-generated method stub
 		ArrayList <Guitar> guitarList= new ArrayList ();
 		Guitar gibson= new Guitar(1,"Les Paul", "Red",1500.35);
-<<<<<<< HEAD
 		System.out.println(gibson.toString());
 		SerializeDemo.writeObject(gibson, "out.txt");
 		Guitar gib=(Guitar) SerializeDemo.readObject("out.txt");
 		System.out.println(gib.getName());
-=======
 		Guitar lucille= new Guitar(1,"es-335", "black",16561500.35);
 		guitarList.add(gibson);
 		guitarList.add(lucille);
-		//System.out.println(gibson.toString());
+		System.out.println(gibson.toString());
 		SerializeDemo.writeObject(guitarList, "out.txt");
 		//SerializeDemo.writeObject(lucille, "out.txt");
 		//Guitar gib= (Guitar)SerializeDemo.readObject("out.txt");
@@ -28,7 +26,6 @@ public class Driver {
 		@SuppressWarnings("unchecked")
 		ArrayList<Guitar> git= (ArrayList<Guitar>)SerializeDemo.readObject("out.txt");
 		System.out.println(git.get(1).getCost());
->>>>>>> 75e86c58ce6404bde7f4fa4ccb4e38a1ced1e338
 	}
 
 }
