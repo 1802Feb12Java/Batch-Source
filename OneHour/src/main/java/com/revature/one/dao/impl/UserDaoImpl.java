7562e742b7d052c40022d3e8afef34ddd1c4610d
Jennifer Fox <jrufox@gmail.com>
2018-03-07 09:51:19 -0500

    2

---
+++ OneHour/src/main/java/com/revature/one/dao/impl/UserDaoImpl.java
@@ -0,0 +1,76 @@
+package com.revature.one.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.revature.one.beans.User;
+import com.revature.one.dao.UserDao;
+import com.revature.one.database.ConnectionFactory;
+
+public class UserDaoImpl implements UserDao {
+
+	public void addUser(User user) throws SQLException {
+		Connection con = null;
+		PreparedStatement s = null;
+		String statement = "CALL INS_REC(?,?)";
+
+		// CommonStatements.InsertIntoFill.apply(TABLE_NAME, TABLE_COLS.length);
+		System.out.println("Created statement = " + statement + "\nwith user " + user);
+
+		try {
+			con = ConnectionFactory.getInstance().getConnection();
+			s = con.prepareStatement(statement);
+			s.setInt(1, user.getId());
+			s.setString(2, user.getName());
+
+			s.executeQuery();
+			System.out.println("Executed SQL Query");
+		} finally {
+			if (s != null)
+				s.close();
+			if (con != null)
+				con.close();
+		}
+
+	}
+
+	public User getUser(int userId) throws SQLException {
+		Connection con = null;
+		PreparedStatement s = null;
+		ResultSet rs = null;
+
+		String statement = "SELECT * FROM ONEHOUR " + " WHERE ID=?" + " ";
+		System.out.println("Created SQL Statement: " + statement + " With USER_ID " + userId);
+
+		try {
+			con = ConnectionFactory.getInstance().getConnection();
+			s = con.prepareStatement(statement);
+			s.setInt(1, userId);
+
+			rs = s.executeQuery();
+			System.out.println("Fetched User with userId: " + userId + " from Database");
+
+			if (rs.next())
+				return processRow(rs);
+		} finally {
+			if (s != null)
+				s.close();
+			if (rs != null)
+				rs.close();
+			if (con != null)
+				con.close();
+		}
+		return null;
+	}
+
+	private User processRow(ResultSet rs) throws SQLException {
+		User user = new User();
+		user.setId(rs.getInt("ID"));
+		user.setName(rs.getString("NAME"));
+		return user;
+
+	}
+
+}
---
+++ OneHour/src/main/java/com/revature/one/database/ConnectionFactory.java
@@ -0,0 +1,63 @@
+package com.revature.one.database;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+	private static ConnectionFactory cf = null;
+
+	private ConnectionFactory() {
+		super();
+
+	}
+
+	public static synchronized ConnectionFactory getInstance() {
+
+		if (cf == null) {
+
+			cf = new ConnectionFactory();
+
+		}
+
+		return cf;
+
+	}
+
+	public Connection getConnection() {
+
+		Connection conn = null;
+
+		Properties prop = new Properties();
+		try {
+			// war file format
+			// prop.load(new FileReader("database.properties"));
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream("database.properties");
+			// BufferedReader reader = new BufferedReader(new
+			// InputStreamReader(inputStream));
+			prop.load(inputStream);
+			try {
+				Class.forName(prop.getProperty("driver"));
+			} catch (ClassNotFoundException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("usr"),
+						prop.getProperty("pwd"));
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+
+	}
+}
