20d587f4af928857c0f56d19342bfdfe8e648898
Jennifer Fox <jrufox@gmail.com>
2018-03-07 11:02:30 -0500

    2

--- OneHour/src/main/java/com/revature/one/servlets/userServlet2.java
+++ OneHour/src/main/java/com/revature/one/servlets/userServlet2.java
@@ -1,11 +1,17 @@
 package com.revature.one.servlets;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.revature.one.beans.User;
+import com.revature.one.dao.impl.UserDaoImpl;
 
 /**
  * Servlet implementation class userServlet2
@@ -27,15 +33,20 @@ public class userServlet2 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// User user = null;
-		// UserDaoImpl ud = new UserDaoImpl();
-		// JSONObject jObj = new JSONObject();
-		//
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
-		// int id = Integer.parseInt(request.getHeader("personID"));
-		// user = ud.getUser(id);
-		//
-		// jObj.jObj.valueToString(user);
+		User user = null;
+		UserDaoImpl ud = new UserDaoImpl();
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int id = Integer.parseInt(request.getHeader("personID"));
+		try {
+			user = ud.getUser(id);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		JSONObject jsonUser = new JSONObject(user);
+		// PrintWriter out = request
 
 	}
 
