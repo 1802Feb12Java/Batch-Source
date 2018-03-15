81a8804b93360e44f777ebdd5344db4e9df107e2
sgm4789 <sgm4789@gmail.com>
2018-03-15 14:02:55 -0400

    Added searchbar functionality for manager

--- EmployeeReimbursementSystem/src/main/webapp/resources/js/ManagerReimbursementView.js
+++ EmployeeReimbursementSystem/src/main/webapp/resources/js/ManagerReimbursementView.js
@@ -1,3 +1,6 @@
+
+var reimbursementInfo;
+
 //the argument magic holds the results
 function loadReimbursements(reimbursementInfo){
     
@@ -37,6 +40,44 @@ function loadReimbursements(reimbursementInfo){
 	
 }
 
+function search(){
+	var myNode = document.getElementById("tableBody");
+	while (myNode.firstChild) {
+	    myNode.removeChild(myNode.firstChild);
+	}
+	
+	for(i = 0; i < reimbursementInfo.length; i++){
+		if(document.getElementById('search').value == reimbursementInfo[i].userIDAuthorString){
+			var table = document.getElementById("tableBody");
+	        var row = table.insertRow(i);
+	        
+	        var cell1 = row.insertCell(0);
+	        var cell2 = row.insertCell(1);
+	        var cell3 = row.insertCell(2);
+	        var cell4 = row.insertCell(3);
+	        var cell5 = row.insertCell(4);
+	        var cell6 = row.insertCell(5);
+	        var cell7 = row.insertCell(6);
+	        var cell8 = row.insertCell(7);
+	        var cell9 = row.insertCell(8);
+	        
+	        cell1.innerHTML = reimbursementInfo[i].userIDAuthorString;
+	        cell2.innerHTML = reimbursementInfo[i].amount;
+	        cell3.innerHTML = "RECEIPT";
+	        cell4.innerHTML = reimbursementInfo[i].reimbursementTypeIDString;
+	        cell5.innerHTML = reimbursementInfo[i].description;
+	        cell6.innerHTML = reimbursementInfo[i].submitted;
+	        cell7.innerHTML = reimbursementInfo[i].userIDResolverString;
+	        cell8.innerHTML = reimbursementInfo[i].reimbursementTypeIDString;
+	        cell9.innerHTML = 
+	        	"<form method = \"post\" action=\"UpdateReimbursementServlet\" class=\"form-horizontal\">" +
+	        		"<div><button name = \"approve\" type=\"submit\" class=\"btn btn-primary\" value = "+reimbursementInfo[i].reiumbursementID+">Approve</button></div>" +
+	        		"<button name = \"deny\" type=\"submit\" class=\"btn btn-primary\" value = "+reimbursementInfo[i].reiumbursementID+">Deny</b>" +
+	        	"</form>"
+		}
+	}
+}
+
 function getReimbursement(){
     //get the information that was submitted in the "magicID" input field
 
@@ -47,7 +88,7 @@ function getReimbursement(){
     xhr.onreadystatechange= function(){
         if(xhr.readyState==4 && xhr.status== 200){
             console.log(xhr.responseText);
-            var reimbursementInfo = JSON.parse(xhr.responseText);
+            reimbursementInfo = JSON.parse(xhr.responseText);
             loadReimbursements(reimbursementInfo);
         }
     }
