5a1cad800f22911d3f3e849952d9d5461a31b234
Ryuujin <ryuujin@asora17.10k>
2018-04-02 23:35:28 -0400

    hello soap & misc stuff

--- BakerR/MavenJDBCTest/src/main/java/mavenJdbcTest/Main.java
+++ BakerR/MavenJDBCTest/src/main/java/mavenJdbcTest/Main.java
@@ -6,30 +6,27 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.Properties;
 
 public class Main {
-	public static void main(String[] args) throws SQLException, IOException {
+	public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
 		Connection conn = null;
 		PreparedStatement ps = null;
 		CallableStatement cs = null;
+		ResultSet rs = null;
 		
 		try {
-	//		Class.forName("org.postgresql.Driver");
+			Class.forName("org.postgresql.Driver");
 			Properties props;
 
 			props = new Properties();
-//			String url = "jdbc:postgresql://pgsql.chpmycag1sts.us-east-2.rds.amazonaws.com:5432/PGSQL";
-//			props.setProperty("user", "wamuu");
-//			props.setProperty("password", "mezametamae");
-//			props.setProperty("ssl", "true");
-//			props.setProperty("sslmode", "verify-full");
-//			props.setProperty("sslrootcert", "/home/ryuujin/employment/revature/repos/Batch-Source/BakerR/project1/ReimbursementSystem/src/main/resources/rds-combined-ca-bundle.pem");
 			
-			InputStream is = Main.class.getClassLoader().getResourceAsStream("database.properties");
-			props.load(is);
+			try(InputStream is = Main.class.getClassLoader().getResourceAsStream("database.properties")) {
+				props.load(is);
+			}
 			
 			conn = DriverManager.getConnection(props.getProperty("host"), props);
 			if(conn.isClosed()) {
@@ -37,27 +34,25 @@ public class Main {
 			} else {
 				System.out.println("Connection is open");
 			}
-			Properties p = conn.getClientInfo();
-			p.stringPropertyNames().forEach((String propName) -> {
-				System.out.println(propName + ": " + p.getProperty(propName));
-			});
 			
-			// Test registering a user with a function.
-	//		CallableStatement reguser = conn.prepareCall("{? = call reguser()}");
-	//		
-	//		boolean status = reguser.execute();
-	//		System.out.println("Reguser call status: " + status);
 			
-			cs = conn.prepareCall("{? = call verify_pass(?, ?)}");
-			cs.registerOutParameter(1, Types.BOOLEAN);
-			cs.setString(2, "esidisi");
-			cs.setString(3, "heat");
-			cs.execute();
+
+			ps = conn.prepareStatement("INSERT INTO test VALUES(?)");
+			ps.setString(1, "山田源氏");
+//			rs = ps.executeQuery();
 			
-			Boolean isValid = cs.getBoolean(1);
+//			ResultSetMetaData rsMeta = rs.getMetaData();
+//			
+//			while(rs.next()) {
+//				System.out.println(rsMeta.getColumnName(1) + ": " + rs.getString(1));
+//			}
 			
-			System.out.println("Verify Pass: " + isValid);
+			
+			
+			
 		} finally {
+			if(rs != null) rs.close();
+			
 			if(ps != null) {
 				ps.close();
 			} 
