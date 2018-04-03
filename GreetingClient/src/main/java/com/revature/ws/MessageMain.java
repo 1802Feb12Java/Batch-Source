3c5cd9c68341fa2183730b3decab5c313f5d9d6a
Kevin Green <kgreen@nevada.unr.edu>
2018-04-02 23:36:03 -0400

    done

---
+++ GreetingClient/src/main/java/com/revature/ws/MessageMain.java
@@ -0,0 +1,13 @@
+package com.revature.ws;
+
+public class MessageMain {
+
+	public static void main(String[] args) {
+		MessageGenService msgen = new MessageGenService();
+		
+		MessageGen msport = msgen.getMessageGenImplPort();
+		
+		System.out.println(msport.getMessage());
+	}
+
+}
